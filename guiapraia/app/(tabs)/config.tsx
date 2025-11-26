91d478c8014c37c7b5d659e1af6c992995525a19
gustavo5300 <antunesggustavo@gmail.com>
2025-11-26 18:28:27 +0000

    V0.0.7

--- guiapraia/app/(tabs)/config.tsx
+++ guiapraia/app/(tabs)/config.tsx
@@ -82,7 +82,7 @@ setModalVisible(!isModalVisible);
 <View style={styles.modalOverlay}>
 {/* Caixa de conteúdo do modal */}
 <View style={styles.modalContent}>
-<Text style={styles.modalText}>Desenvolvido por: Aluno</Text>
+<Text style={styles.modalText}>Desenvolvido por: Gustavo Antunes - 2025</Text>
 <TouchableOpacity
 
 style={styles.modalCloseButton}
